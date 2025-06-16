a3636e6538d3d509514d978a5f400f61a6d0774c
Danbradleystakusopere <danstakus@gmail.com>
2025-06-16 10:15:36 +0300

    updated

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Remove existing main element
+const main = document.getElementById('main');
+main.remove();
+
+// Create new h1 element
+const newHeader = document.createElement('h1');
+
+// Set id to "victory"
+newHeader.id = 'victory';
+
+// Set content
+newHeader.textContent = 'Danbradley is the champion';
+
+// Append to body
+document.body.append(newHeader);
